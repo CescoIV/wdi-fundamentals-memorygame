db6bd8e5afb3ef50cc0c4d34f514852dfc5c0491
Fran <crordaz@gmail.com>
2017-08-04 17:16:05 -0500

    Finalized game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,26 +22,57 @@ var cards = [
 
 ];
 var inPlay = [];
+
+var pairs = 0;
 function checkMatch(){
-	if(inPlay[0] === inPlay[1]){
-		alert("You found a match!");
+	//checks the last two cards logged into the inPlay array
+	if(inPlay[inPlay.length-2] === inPlay[inPlay.length-1]){
+		pairs++;
+		alert("You found a match! Score: " + pairs);
+		var ayy = document.getElementById('score');
+		ayy.textContent = 'Score: ' + pairs
 	}else{
 		alert("Sorry try again.");
 	}
 }
 
-function cardFlip(cardId){
-	var card = cards[cardId];
+function cardFlip(){
+	//this.getAttribute('data-id');
+	var card = cards[this.getAttribute('data-id')];
 	console.log("User flipped: " + card.rank);
 	console.log(card.suit);
 	console.log(card.cardImage);
-	
+
 	inPlay.push(card.rank)
 
-	if(inPlay.length === 2){
+	this.setAttribute('src', card.cardImage);
+	if(inPlay.length%2 === 0){
 		checkMatch(); 
 	}
 }
+//Durstenfeld Shuffle
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
-cardFlip(0);
-cardFlip(2);
+function createBoard(){
+	shuffleArray(cards);
+	for(i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+
+		cardElement.addEventListener('click', cardFlip);
+		document.getElementById('game-board').appendChild(cardElement); 
+	}
+}
+
+document.getElementById('r2').addEventListener('click', function(){
+	createBoard();
+});
